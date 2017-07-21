d1ae6c1c13192225b98f8b9ba48fbdd588463784
Cache Staheli <cache@byu.edu>
2017-07-21 02:15:04 +0000

    Add config and basic foursquare

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
+var secrets = config.get('foursquareApi.secrets');
+var foursquare = require('node-foursquare')(secrets);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res) {
+	res.writeHead(303, { 'location': foursquare.getAuthClientRedirectUrl() });
+	res.end();
+});
+
+app.get('/callback', function (req, res) {
+	foursquare.getAccessToken({
+    		code: req.query.code
+  	}, function (error, accessToken) {
+    		if(error) {
+      			res.send('An error was thrown: ' + error.message);
+    		}
+    		else {
+      			// Save the accessToken and redirect.
+    		}
+  	});
 });
 
 module.exports = router;
